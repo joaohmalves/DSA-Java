7b3c7f1a12ee3cfb0d600844be882ee1f6b73be4
joaohmalves <123655649+joaohmalves@users.noreply.github.com>
2025-03-13 15:53:30 -0300

    Add files via upload

---
+++ Solutions LeetCode/867. Transpose Matrix/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int[][] teste = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                teste[j][i] = matrix[i][j];
+            }
+        }
+
+        return teste;
+    }
+}
